f44ea5097ae711cf7210d7fffcd7b690fe9eb43e
James <james@jameshawkins.com>
2019-04-15 13:26:36 -0700

    Cleanup unused code

--- src/welcome.js
+++ src/welcome.js
@@ -31,18 +31,6 @@ function Welcome(context) {
     Welcome.prototype.zombies = () => {
         context.fillText(`don't let the zombies catch you.`, 0, 400);
     }
-    // Welcome.prototype.controls = () => {
-    //     context.clearRect(0, 0, canvasWidth, canvasHeight);
-    //     context.font="45px Arima Madurai";
-    //     context.fillStyle = "#b7b7b7";
-    //     context.fillText(`controls`, canvasWidth/2-80, 100);
-    //     context.font="30px Arima Madurai";
-    //     context.fillText(`up (w)`, canvasWidth/2-45, 200);
-    //     context.fillText(`left (a) / down (s) / right (d)`, canvasWidth/2-180, 250);
-    //     context.fillText(`look around (mouse)`, canvasWidth/2-130, 350);
-    //     context.fillText(`sprint (space)`, canvasWidth/2-80, 400);
-    //     context.fillText(`items (numbers 1-9)`, canvasWidth/2-130, 500);
-    // }
 
     Welcome.prototype.draw = () => {
         this.welcome();
